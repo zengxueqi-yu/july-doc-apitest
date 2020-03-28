a2f966d380cf7c36c0e80a5c27454aa7bf48545c
zengxueqi-yu@outlook.com <zengxueqi-yu@outlook.com>
2020-03-28 09:32:58 +0800

    july doc在线调试工具

---
+++ src/main/java/com/api/apitest/controller/ApiTestController.java
@@ -0,0 +1,38 @@
+package com.api.apitest.controller;
+
+import com.api.apitest.config.ApiResult;
+import com.api.apitest.config.Constants;
+import com.api.apitest.entity.ApiRequestParam;
+import com.api.apitest.utils.ApiTestUtil;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 自动化工具接口
+ * @author zqk
+ * @since 2020/2/3
+ */
+@RestController
+@RequestMapping("/api")
+public class ApiTestController {
+
+    /**
+     * 自动化工具方法
+     * @param: [apiParam]
+     * @return: ApiResult<java.lang.String>
+     * @author: zengxueqi
+     * @since: 2020/2/25
+     */
+    @PostMapping("/test")
+    public ApiResult<String> apiTest(@RequestBody ApiRequestParam apiParam) {
+        if(Constants.REQUEST_GET.equals(apiParam.getMethod())){
+            return ApiResult.ok(ApiTestUtil.httpGet(apiParam));
+        }else{
+            return ApiResult.ok(ApiTestUtil.httpPost(apiParam));
+        }
+
+    }
+
+}
---
+++ src/main/java/com/api/apitest/controller/IndexController.java
@@ -0,0 +1,34 @@
+package com.api.apitest.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * 自动化测试工具入口
+ * @author zqk
+ * @since 2020/2/3
+ */
+@Controller
+public class IndexController {
+
+    /**
+     * 首页
+     * @param
+     * @return java.lang.String
+     * @author zengxueqi
+     * @since 2020/2/26
+     */
+    @RequestMapping("/api")
+    public String test(){
+        return "/api";
+    }
+
+    @GetMapping("/testapi")
+    @ResponseBody
+    public String testapi(String name){
+        return name;
+    }
+
+}
